a842be6bd920e2d370e34a368d959f9ad7dcdbc1
adam_zhen <adam.ray.zheng@gmail.com>
2022-10-22 17:37:15 -0400

    update

--- dmopc18c6p3_v2.cpp
+++ dmopc18c6p3_v2.cpp
@@ -75,17 +75,6 @@ int init(Graph* g)
     return count;
 }
 
-std::vector<std::string> split (const std::string &s, char delim) {
-    std::vector<std::string> result;
-    std::stringstream ss (s);
-    std::string item;
-
-    while (getline (ss, item, delim)) {
-        result.push_back (item);
-    }
-
-    return result;
-}
 
 int N, M;
 
